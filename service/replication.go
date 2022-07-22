e66def64dce15ed823c50715876e729743f68c59
Alina Mullagalieva <alina.mullagalieva@dell.com>
2022-07-22 07:11:25 -0700

    added get spg status

--- service/replication.go
+++ service/replication.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"strings"
 
+	"github.com/dell/gounity/types"
+
 	csiext "github.com/dell/dell-csi-extensions/replication"
 	"github.com/dell/gounity"
 	"google.golang.org/grpc/codes"
@@ -112,20 +114,21 @@ func (s *service) CreateStorageProtectionGroup(ctx context.Context, req *csiext.
 			return nil, err
 		}
 		remoteProtectionGroupID := strings.Split(req.VolumeHandle, "=_=")[0]
-		localProtectionGroupID := strings.ReplaceAll(remoteProtectionGroupID, strings.ToUpper(req.Parameters[s.WithRP(keyReplicationRemoteSystem)]), strings.ToUpper(req.Parameters[(keyArrayID)]))
 		localParams := map[string]string{
 			s.opts.replicationContextPrefix + "systemName":       arrayID,
 			s.opts.replicationContextPrefix + "remoteSystemName": replSession.ReplicationSessionContent.RemoteSystem.Name,
 			s.opts.replicationContextPrefix + "VolumeGroupName":  fileSystems.FileContent.Name,
+			s.opts.replicationContextPrefix + "protocol":         protocol,
 		}
 		remoteParams := map[string]string{
 			s.opts.replicationContextPrefix + "systemName":       replSession.ReplicationSessionContent.RemoteSystem.Name,
 			s.opts.replicationContextPrefix + "remoteSystemName": arrayID,
 			s.opts.replicationContextPrefix + "VolumeGroupName":  fileSystems.FileContent.Name,
+			s.opts.replicationContextPrefix + "protocol":         protocol,
 		}
 		return &csiext.CreateStorageProtectionGroupResponse{
-			LocalProtectionGroupId:          localProtectionGroupID,
-			RemoteProtectionGroupId:         remoteProtectionGroupID,
+			LocalProtectionGroupId:          remoteProtectionGroupID + "::" + arrayID,
+			RemoteProtectionGroupId:         remoteProtectionGroupID + "::" + replSession.ReplicationSessionContent.RemoteSystem.Name,
 			LocalProtectionGroupAttributes:  localParams,
 			RemoteProtectionGroupAttributes: remoteParams,
 		}, nil
@@ -138,10 +141,6 @@ func (s *service) DeleteStorageProtectionGroup(ctx context.Context, req *csiext.
 	localParams := req.GetProtectionGroupAttributes()
 	groupID := req.GetProtectionGroupId()
 	arrayID, ok := localParams[s.opts.replicationContextPrefix+"systemName"]
-	if !ok {
-		log.Error("Can't get systemName from PG params")
-	}
-	remoteArrayID, ok := localParams[s.opts.replicationContextPrefix+"remoteSystemName"]
 	if !ok {
 		log.Error("Can't get systemName from PG params")
 	}
@@ -160,13 +159,16 @@ func (s *service) DeleteStorageProtectionGroup(ctx context.Context, req *csiext.
 
 	log.WithFields(fileds).Info("Deleting storage protection group")
 	fsAPI := gounity.NewFilesystem(localUnity)
-	prefix := strings.ReplaceAll(groupID, strings.ToUpper(arrayID), strings.ToUpper(remoteArrayID))
-	fsGroup, err := fsAPI.FindFileSystemGroupByPrefix(ctx, prefix)
+	prefix := strings.Split(groupID, "::")
+	if len(prefix) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Wrong groupID format")
+	}
+	fsGroup, err := fsAPI.FindFileSystemGroupByPrefixWithFields(ctx, prefix[0])
 	if err != nil {
 		return nil, err
 	}
 
-	if len(fsGroup.Filesystems) != 0 {
+	if len(fsGroup) != 0 {
 		return nil, status.Error(codes.Internal, "FS group is not empty")
 	}
 
@@ -178,11 +180,99 @@ func (s *service) ExecuteAction(ctx context.Context, req *csiext.ExecuteActionRe
 }
 
 func (s *service) GetStorageProtectionGroupStatus(ctx context.Context, req *csiext.GetStorageProtectionGroupStatusRequest) (*csiext.GetStorageProtectionGroupStatusResponse, error) {
-	// no SPG, volume groups
-	// check replication session status of all volumes in volume group
-	// if one is bad then all SPG status bad
+	localParams := req.GetProtectionGroupAttributes()
+	arrayID := localParams[s.opts.replicationContextPrefix+"systemName"]
+	protocol := localParams[s.opts.replicationContextPrefix+"protocol"]
+	groupID := req.GetProtectionGroupId()
+	prefix := strings.Split(groupID, "::")
+	if len(prefix) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Wrong groupID format")
+	}
+	vgName := prefix[0]
 
-	return nil, nil
+	ctx, log := setArrayIDContext(ctx, arrayID)
+	if err := s.requireProbe(ctx, arrayID); err != nil {
+		return nil, err
+	}
+
+	if protocol == NFS {
+		unity, err := s.getUnityClient(ctx, arrayID)
+		if err != nil {
+			return nil, err
+		}
+
+		filesystemAPI := gounity.NewFilesystem(unity)
+		listFS, err := filesystemAPI.FindFileSystemGroupByPrefixWithFields(ctx, vgName)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(listFS) != 0 {
+			replAPI := gounity.NewReplicationSession(unity)
+			replSession, err := replAPI.FindReplicationSessionBySrcResourceID(ctx, listFS[0].FileContent.StorageResource.ID)
+			if err != nil {
+				return nil, err
+			}
+			if replSession == nil {
+				log.Error("Replication session doesn't exist")
+				return &csiext.GetStorageProtectionGroupStatusResponse{
+					Status: &csiext.StorageProtectionGroupStatus{State: csiext.StorageProtectionGroupStatus_EMPTY},
+				}, nil
+			}
+
+			rsStatus := replSession.ReplicationSessionContent.Status
+			//We compare with 0 because 0 means source in unity api
+			isSource := replSession.ReplicationSessionContent.LocalRole == 0
+
+			breakParam := false
+			for i, fs := range listFS {
+				if i == 0 {
+					continue
+				}
+				replSession, err = replAPI.FindReplicationSessionBySrcResourceID(ctx, fs.FileContent.StorageResource.ID)
+				if err != nil {
+					return nil, err
+				}
+				if rsStatus != replSession.ReplicationSessionContent.Status {
+					breakParam = true
+					break
+				}
+			}
+
+			if breakParam == true {
+				return &csiext.GetStorageProtectionGroupStatusResponse{
+					Status: &csiext.StorageProtectionGroupStatus{State: csiext.StorageProtectionGroupStatus_UNKNOWN, IsSource: isSource},
+				}, nil
+			}
+
+			var monitoringState csiext.StorageProtectionGroupStatus_State
+			switch rsStatus {
+			case types.RS_STATUS_ACTIVE, types.RS_STATUS_OK:
+				monitoringState = csiext.StorageProtectionGroupStatus_SYNCHRONIZED
+				break
+			case types.RS_STATUS_SYNCING, types.RS_STATUS_MANUAL_SYNCING, types.RS_STATUS_MANUAL_SYNCING_MIXED:
+				monitoringState = csiext.StorageProtectionGroupStatus_SYNC_IN_PROGRESS
+				break
+			case types.RS_STATUS_PAUSED, types.RS_STATUS_PAUSED_MIXED:
+				monitoringState = csiext.StorageProtectionGroupStatus_SUSPENDED
+				break
+			case types.RS_STATUS_FAILED_OVER, types.RS_STATUS_FAILED_OVER_MIXED, types.RS_STATUS_FAILED_OVER_WITH_SYNC, types.RS_STATUS_FAILED_OVER_WITH_SYNC_MIXED:
+				monitoringState = csiext.StorageProtectionGroupStatus_FAILEDOVER
+				break
+			case types.RS_STATUS_LOST_COMMUNICATION, types.RS_STATUS_LOST_SYNC_COMMUNICATION, types.RS_STATUS_NON_RECOVERABLE_ERROR:
+				monitoringState = csiext.StorageProtectionGroupStatus_INVALID
+				break
+			default:
+				monitoringState = csiext.StorageProtectionGroupStatus_UNKNOWN
+				break
+			}
+
+			return &csiext.GetStorageProtectionGroupStatusResponse{
+				Status: &csiext.StorageProtectionGroupStatus{State: monitoringState, IsSource: isSource},
+			}, nil
+		}
+	}
+	return nil, status.Error(codes.Unimplemented, "Block replication is not implemented")
 }
 
 // WithRP appends Replication Prefix to provided string
